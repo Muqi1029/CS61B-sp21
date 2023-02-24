c1c787c0ce9c68b9553f2491baf46052e882b2ee
mq <642733045@qq.com>
2023-02-24 08:46:53 +0800

    modify initially

--- proj0/game2048/Game.java
+++ proj0/game2048/Game.java
@@ -6,7 +6,7 @@ import static game2048.Side.*;
  *  @author P. N. Hilfinger. */
 public class Game {
 
-    /** Controller for a game represented by MODEL, using SOURCE as the
+    /** Controller for a game represented by MODEL, using SOURCE as
      *  the source of key inputs and random Tiles. */
     public Game(Model model, InputSource source) {
         _model = model;
